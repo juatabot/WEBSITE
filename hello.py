df1e83228fb2f974d22ccf262ce5087622d6c3d5
Juat <hengjuat@gmail.com>
2020-04-27 19:16:39 -0400

    Set up Flask to serve React app

--- hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from routes import index
-app = Flask(__name__)
-
-import routes
-
-app.register_blueprint(index)
-
--- routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-index = Blueprint('index', __name__)
-
-@index.route('/')
-def hello_world():
-    return 'Hello, World!'
